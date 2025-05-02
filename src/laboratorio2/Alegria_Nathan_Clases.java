2a66d3f12abf8768b403b86cf26efbe24f411343
Nathan Alegria <nathanalegria02@gmail.com>
2025-05-02 08:41:05 -0600

    Inicio trabajo

---
+++ src/laboratorio2/Alegria_Nathan_Clases.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio2;
+
+/**
+ *
+ * @author Nathan
+ */
+public class Alegria_Nathan_Clases {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+    
+}
