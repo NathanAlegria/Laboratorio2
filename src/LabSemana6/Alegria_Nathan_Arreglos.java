4bb54c022391972061c3ff11e2bb942dec141a52
Nathan Alegria <nathanalegria02@gmail.com>
2025-05-30 11:22:25 -0600

    Inicio

---
+++ src/LabSemana6/Alegria_Nathan_Arreglos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LabSemana6;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author Nathan
+ */
+public class Alegria_Nathan_Arreglos {
+    public static void main(String[] args) {
+        Scanner lea=new Scanner(System.in);
+        System.out.println("Menu Opciones");
+        System.out.println("a.Jugar"+"\nb.Cambiar Palabras");
+        System.out.println("Seleccionar Opción");
+        String opcion=lea.next();
+        do{
+            
+        }while(opcion!=);
+    }
+}
